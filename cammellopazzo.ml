30070d83a43c556d3f0289c592bac844e0cba8ee
Leonardo Scoppitto <70918126+Skiby7@users.noreply.github.com>
2020-12-18 18:15:50 +0100

    started testing

--- cammellopazzo.ml
+++ cammellopazzo.ml
@@ -8,7 +8,7 @@ let bind (r : 't env) (i : ide) (v : 't) = function x -> if x = i then v else ap
 
 type exp = 
 		(*Valori primitivi*)
-			Eint of int 
+		  Eint of int 
 		| Ebool of bool 
 		| Estring of string
 		(*Operatori già inclusi nel linguaggio*)
@@ -68,14 +68,14 @@ and evFun = ide * exp * evT env;;
 (*type checking*)
 let typecheck (s : string) (v : evT) : bool = match s with
 		"int" -> (match v with
-									Int(_) -> true 
-								| _ -> false) 
+			  Int(_) -> true 
+			| _ -> false) 
 	| "bool" -> (match v with
-									Bool(_) -> true 
-								| _ -> false) 
+			  Bool(_) -> true 
+			| _ -> false) 
 	| "string" -> (match v with
-								String(_) -> true
-							|_ ->false)
+			  String(_) -> true
+			| _ ->false)
 	| _ -> failwith("not a valid type");;
 
 (*funzioni primitive*)
@@ -114,8 +114,8 @@ let et x y = if (typecheck "bool" x) && (typecheck "bool" y)
 
 let non x = if (typecheck "bool" x)
 	then (match x with
-						Bool(true) -> Bool(false) 
-					| Bool(false) -> Bool(true))
+	   	  Bool(true) -> Bool(false) 
+		| Bool(false) -> Bool(true))
 	else failwith("Type error");;
 	
 			(*Funzione di supporto per la conversione di tipo*)
@@ -130,28 +130,28 @@ let toexp (a : evT) = match a with
 (*Alcune funzioni base *)
 
 let rec subaux (l : 'a list)(m : 'a list) : bool = match l, m with
-							  [], _ -> true
-							| _, [] -> false
-							| hdl::tll, hdm::tlm -> hdl = hdm && subaux tll tlm;;
+			  [], _ -> true
+			| _, [] -> false
+			| hdl::tll, hdm::tlm -> hdl = hdm && subaux tll tlm;;
 
 let rec sublist (l : 'a list)(m : 'a list) : bool = match l, m with
-							  [], _ -> true
-							| _, [] -> false
-							| hdl::_, hdm::tlm -> hdl = hdm && subaux l m || sublist l tlm;;											
+			  [], _ -> true
+			| _, [] -> false
+			| hdl::_, hdm::tlm -> hdl = hdm && subaux l m || sublist l tlm;;											
 
 let rec contains (toSearch : 'a)(l : 'a list) : bool = match l with
-                                                    	  [] -> false
-							| element::tail -> element = toSearch || contains toSearch tail;;
+			  [] -> false
+			| element::tail -> element = toSearch || contains toSearch tail;;
 
 let rec remove_from_list (element : 'a)(l : 'a list) : 'a list = match l with
-							  [] -> []
-							| head::tail -> if head = element then remove_from_list element tail 
-									else head::(remove_from_list element tail);;
+			  [] -> []
+			| head::tail -> if head = element then remove_from_list element tail 
+					 else head::(remove_from_list element tail);;
 
 let rec list_as_set (l : 'a list) : 'a list = match l with
-							  [] -> []
-							| head::tail -> if (contains head tail) then head::list_as_set((remove_from_list head tail)) 
-															else head::(list_as_set tail);;
+			  [] -> []
+			| head::tail -> if (contains head tail) then head::list_as_set((remove_from_list head tail)) 
+					 else head::(list_as_set tail);;
 (*
 let rec forall (f : exp) (l : evT list) : evT = match l with
 																						[] -> Bool(true)
@@ -178,64 +178,64 @@ let rec eval (e : exp) (r : evT env) : evT = match e with
 		| And(a, b) -> et (eval a r) (eval b r) 
 		| Or(a, b) -> vel (eval a r) (eval b r) 
 		| Not a -> non (eval a r) 
-		| Ifthenelse(a, b, c) -> 	let g = (eval a r) in
+		| Ifthenelse(a, b, c) -> let g = (eval a r) in
 				if (typecheck "bool" g) 
 					then (if g = Bool(true) then (eval b r) else (eval c r))
 					else failwith ("nonboolean guard") 
-		|	Let(i, e1, e2) -> eval e2 (bind r i (eval e1 r)) 
+		| Let(i, e1, e2) -> eval e2 (bind r i (eval e1 r)) 
 		| Fun(i, a) -> FunVal(i, a, r) 
 		| FunCall(f, eArg) -> let fClosure = (eval f r) in
 				(match fClosure with
-						  FunVal(arg, fBody, fDecEnv) -> eval fBody (bind fDecEnv arg (eval eArg r)) 
-						| RecFunVal(g, (arg, fBody, fDecEnv)) -> let aVal = (eval eArg r) in
-																	let rEnv = (bind fDecEnv g fClosure) in
-																		let aEnv = (bind rEnv arg aVal) in
-																				eval fBody aEnv 
-						|	_ -> failwith("non functional value")) 
+					  FunVal(arg, fBody, fDecEnv) -> eval fBody (bind fDecEnv arg (eval eArg r)) 
+					| RecFunVal(g, (arg, fBody, fDecEnv)) -> let aVal = (eval eArg r) in
+										  let rEnv = (bind fDecEnv g fClosure) in
+										    let aEnv = (bind rEnv arg aVal) in
+											eval fBody aEnv 
+					|	_ -> failwith("non functional value")) 
 						
 		| Letrec(f, funDef, letBody) ->(match funDef with
-										Fun(i, fBody) -> let r1 = (bind r f (RecFunVal(f, (i, fBody, r)))) in
-															eval letBody r1 
-									  | _ -> failwith("non functional def"))
+						  Fun(i, fBody) -> let r1 = (bind r f (RecFunVal(f, (i, fBody, r)))) in eval letBody r1 
+															
+						| _ -> failwith("non functional def"))
 	
 		(*=================== operazioni di base ====================*)
 					(*Creazione insiemi*)
 		| Empty(type_) ->( match type_ with
-													"int" -> SetVal([], type_)
-												| "bool" -> SetVal([], type_)
-												| "string" -> SetVal([], type_)
-												| _ -> failwith("Not a valid type"))
+				  "int" -> SetVal([], type_)
+				| "bool" -> SetVal([], type_)
+				| "string" -> SetVal([], type_)
+				| _ -> failwith("Not a valid type"))
 
 		| Singleton(a, type_) -> if typecheck type_ (eval a r) then SetVal((eval a r)::[], type_) else failwith("Wrong type")
 					(*Operazioni elementari*)
 		| Union(s1, s2) -> (match (eval s1 r), (eval s2 r) with
-													SetVal(l1, t1), SetVal(l2, t2) -> if t1 <> t2 then failwith("Sets are not the same type") 
-																															else let final_list = l1@l2 in SetVal(list_as_set final_list, t1)
-												| _,SetVal(_,_) -> failwith("s1 is not a Set")
-												| SetVal(_,_),_ -> failwith("s2 is not a Set")
-												| _,_ -> failwith("Not a valid pair of Sets"))
+					  SetVal(l1, t1), SetVal(l2, t2) -> if t1 <> t2 then failwith("Sets are not the same type") 
+					  					else let final_list = l1@l2 in SetVal(list_as_set final_list, t1)
+					| _,SetVal(_,_) -> failwith("s1 is not a Set")
+					| SetVal(_,_),_ -> failwith("s2 is not a Set")
+					| _,_ -> failwith("Not a valid pair of Sets"))
 		| Intersection(s1, s2) -> (match (eval s1 r), (eval s2 r) with
-													SetVal(l1, t1), SetVal(l2, t2) ->( if t1 <> t2 then failwith("Sets are not the same type") 
-																															else let rec intersect l1 l2 = match l1, l2 with
-																																					head::tail, lista -> if (contains head lista) then head::(intersect tail lista) else (intersect tail lista)
-																																				| [], lista -> []
-																																				| lista, [] -> []	
-																																				in SetVal(intersect l1 l2, t1))
-												| _,SetVal(_,_) -> failwith("s1 is not a Set")
-												| SetVal(_,_),_ -> failwith("s2 is not a Set")
-												| _,_ -> failwith("Not a valid pair of Sets"))
+					  SetVal(l1, t1), SetVal(l2, t2) ->( if t1 <> t2 then failwith("Sets are not the same type") 
+										else let rec intersect l1 l2 = match l1, l2 with
+												  head::tail, lista -> if (contains head lista) then head::(intersect tail lista) 
+															else (intersect tail lista)
+												| [], lista -> []
+												| lista, [] -> []	
+											in SetVal(intersect l1 l2, t1))
+					| _,SetVal(_,_) -> failwith("s1 is not a Set")
+					| SetVal(_,_),_ -> failwith("s2 is not a Set")
+					| _,_ -> failwith("Not a valid pair of Sets"))
 		| Difference(s1, s2) ->( match (eval s1 r), (eval s2 r) with
-											SetVal(l1, t1), SetVal(l2, t2) ->( if t1 <> t2 then failwith("Sets are not the same type") 
-																													else let rec difference l1 l2 = match l1, l2 with
-																													lista, head::tail -> difference (remove_from_list head lista) tail
-																												| [], lista -> []
-																												| lista, [] -> lista
-																												in SetVal(difference l1 l2, t1))
-																												
-																												
-											| _,SetVal(_,_) -> failwith("s1 is not a Set")
-											| SetVal(_,_),_ -> failwith("s2 is not a Set")
-											| _,_ -> failwith("Not a valid pair of Sets"))
+					  SetVal(l1, t1), SetVal(l2, t2) ->( if t1 <> t2 then failwith("Sets are not the same type") 
+										else let rec difference l1 l2 = match l1, l2 with
+												  lista, head::tail -> difference (remove_from_list head lista) tail
+												| [], lista -> []
+												| lista, [] -> lista
+											in SetVal(difference l1 l2, t1))
+
+					| _,SetVal(_,_) -> failwith("s1 is not a Set")
+					| SetVal(_,_),_ -> failwith("s2 is not a Set")
+					| _,_ -> failwith("Not a valid pair of Sets"))
 
   				(*Operazioni base di cui è richiesta l'implementazione*)
 		| Insert (s, toAdd) -> (
@@ -244,116 +244,113 @@ let rec eval (e : exp) (r : evT env) : evT = match e with
                                                 else failwith("Already Existing")) else failwith ("Wrong type")
                             | _-> failwith("Not a Set"))
 
-    | IsIn (s, query) -> (
+    		| IsIn (s, query) -> (
                           match eval s r with
                             SetVal(l, t) -> if (typecheck t (eval query r)) then Bool(contains (eval query r) l) else failwith ("Wrong type")
-      | _-> failwith("Not a Set"))
+      			  | _-> failwith("Not a Set"))
     
-    | Rm (s, toDel) -> (
+    		| Rm (s, toDel) -> (
                         match eval s r with
                           SetVal(l, t) -> if (typecheck t (eval toDel r)) then SetVal(delete toDel l, t) else failwith ("Wrong type")
                         | _-> failwith("Not a Set"))
 
-  	| Getmin (s) -> (match eval s r with
-                        SetVal(l, t) -> findmin l
-											| _-> failwith("Not a Set"))
+  		| Getmin (s) -> (match eval s r with
+                          SetVal(l, t) -> findmin l
+			| _-> failwith("Not a Set"))
 											
 		| Getmax (s) ->( match eval s r with
-											SetVal(l, t) -> findmax l
-										| _-> failwith("Not a Set"))
+			  SetVal(l, t) -> findmax l
+			| _-> failwith("Not a Set"))
 
 		| IsEmpty (s) -> (match eval s r with
-												SetVal(l, t) -> (match l with
-																			| [] -> Bool(true)
-																			| h::t -> Bool(false)
-																			|_ -> failwith("Not a valid Set"))
-
-												| _ -> failwith("Not a Set"))
+			  SetVal(l, t) -> (match l with
+						  [] -> Bool(true)
+						| h::t -> Bool(false)
+						|_ -> failwith("Not a valid Set"))
+			| _ -> failwith("Not a Set"))
 
 		| IsSubset (s1, s2) -> (match (eval s1 r, eval s2 r) with
-																(SetVal(l, t1), SetVal(m, t2)) -> if (t1 = t2) then Bool(sublist l m) else failwith("Sets are not the same type")
-															| _ -> failwith("Not a valid enty")
-															
-															)
+					  (SetVal(l, t1), SetVal(m, t2)) -> if (t1 = t2) then Bool(sublist l m) 
+										else failwith("Sets are not the same type")
+					| _ -> failwith("Not a valid enty"))
   				(*Operazioni di carattere funzionale di cui è richiesta l'implementazione*)
 		
 		| For_all(f, s) -> ( match f, eval s r with
-														f, SetVal([], t) -> failwith("Set is Empty")
-													| f, SetVal(l, t) ->  let fClosure = (eval f r) 
-																									in let apply (f : evT) (v : evT) : evT = (match f with
-																FunVal(arg, fBody, fDecEnv) -> eval fBody (bind fDecEnv arg v) 
-															| RecFunVal(g, (arg, fBody, fDecEnv)) -> let rEnv = (bind fDecEnv g f) in
-																																					let aEnv = (bind rEnv arg v) in
-																																											eval fBody aEnv 
-															|	_ -> failwith("non functional value"))
-																								in let rec forall  (f : evT) (lista : evT list) : evT = match lista with
-																																[] -> Bool(true)
-																															|	hd::tl -> if ((apply f hd) = Bool(true)) then forall f tl else Bool(false)
-																							in forall fClosure l
-													| f, _ -> failwith("Not a valid set")
-		)
+					  f, SetVal([], t) -> failwith("Set is Empty")
+					| f, SetVal(l, t) ->  let fClosure = (eval f r) 
+								in let apply (f : evT) (v : evT) : evT = (match f with
+								  FunVal(arg, fBody, fDecEnv) -> eval fBody (bind fDecEnv arg v) 
+								| RecFunVal(g, (arg, fBody, fDecEnv)) -> let rEnv = (bind fDecEnv g f) in
+													  let aEnv = (bind rEnv arg v) in
+														eval fBody aEnv 
+								| _ -> failwith("non functional value"))
+							in let rec forall  (f : evT) (lista : evT list) : evT = match lista with
+											  [] -> Bool(true)
+											| hd::tl -> if ((apply f hd) = Bool(true)) then forall f tl 
+													else Bool(false)
+										in forall fClosure l
+								| f, _ -> failwith("Not a valid set"))
 		| Exists(f, s) -> ( match f, eval s r with
-														f, SetVal([], t) -> failwith("Set is Empty")
-													| f, SetVal(l, t) ->  let fClosure = (eval f r) 
-																							in let apply (f : evT) (v : evT) : evT = (match f with
-																									FunVal(arg, fBody, fDecEnv) -> eval fBody (bind fDecEnv arg v) 
-																								| RecFunVal(g, (arg, fBody, fDecEnv)) -> let rEnv = (bind fDecEnv g f) in
-																																													let aEnv = (bind rEnv arg v) in
-																																															eval fBody aEnv 
-																								|	_ -> failwith("non functional value"))
-																		in let rec exists  (f : evT) (lista : evT list) : evT = match lista with
-																				[] -> Bool(false)
-																			|	hd::tl -> if ((apply f hd) = Bool(true)) then Bool(true) else exists f tl
-											in exists fClosure l
-	| f, _ -> failwith("Not a valid set")
-)
+					  f, SetVal([], t) -> failwith("Set is Empty")
+					| f, SetVal(l, t) ->  let fClosure = (eval f r) 
+								in let apply (f : evT) (v : evT) : evT = (match f with
+								  FunVal(arg, fBody, fDecEnv) -> eval fBody (bind fDecEnv arg v) 
+								| RecFunVal(g, (arg, fBody, fDecEnv)) -> let rEnv = (bind fDecEnv g f) in
+													  let aEnv = (bind rEnv arg v) in
+													  	eval fBody aEnv 
+								|	_ -> failwith("non functional value"))
+							in let rec exists  (f : evT) (lista : evT list) : evT = match lista with		
+											  [] -> Bool(false)
+											| hd::tl -> if ((apply f hd) = Bool(true)) then Bool(true) 
+													else exists f tl
+										in exists fClosure l
+								| f, _ -> failwith("Not a valid set"))
 
-| Filter(f, s) -> ( match f, eval s r with
-														f, SetVal([], t) -> failwith("Set is Empty")
-													| f, SetVal(l, t) ->  let fClosure = (eval f r) 
-																							in let apply (f : evT) (v : evT) : evT = (match f with
-																									FunVal(arg, fBody, fDecEnv) -> eval fBody (bind fDecEnv arg v) 
-																								| RecFunVal(g, (arg, fBody, fDecEnv)) -> let rEnv = (bind fDecEnv g f) in
-																																													let aEnv = (bind rEnv arg v) in
-																																															eval fBody aEnv 
-																								|	_ -> failwith("non functional value"))
-																		in let rec filter  (f : evT) (lista : evT list) : (evT list) = match lista with
-																				[] -> []
-																			|	hd::tl -> if ((apply f hd) = Bool(true)) then hd::(filter f tl) else filter f tl
-											in SetVal(filter fClosure l, t)
-	| f, _ -> failwith("Not a valid set")
-)
+		| Filter(f, s) -> ( match f, eval s r with
+					  f, SetVal([], t) -> failwith("Set is Empty")
+					| f, SetVal(l, t) ->  let fClosure = (eval f r) 
+								in let apply (f : evT) (v : evT) : evT = (match f with
+								  FunVal(arg, fBody, fDecEnv) -> eval fBody (bind fDecEnv arg v) 
+								| RecFunVal(g, (arg, fBody, fDecEnv)) -> let rEnv = (bind fDecEnv g f) in
+													  let aEnv = (bind rEnv arg v) in
+														eval fBody aEnv 
+								|	_ -> failwith("non functional value"))
+							in let rec filter  (f : evT) (lista : evT list) : (evT list) = match lista with
+											  [] -> []
+											| hd::tl -> if ((apply f hd) = Bool(true)) then hd::(filter f tl) 
+													else filter f tl
+										in SetVal(filter fClosure l, t)
+					| f, _ -> failwith("Not a valid set"))
 
-| Map(f, s) -> ( match f, eval s r with
-														f, SetVal([], t) -> failwith("Set is Empty")
-													| f, SetVal(l, t) ->  let fClosure = (eval f r) 
-																							in let apply (f : evT) (v : evT) : evT = (match f with
-																									FunVal(arg, fBody, fDecEnv) -> eval fBody (bind fDecEnv arg v) 
-																								| RecFunVal(g, (arg, fBody, fDecEnv)) -> let rEnv = (bind fDecEnv g f) in
-																																													let aEnv = (bind rEnv arg v) in
-																																															eval fBody aEnv 
-																								|	_ -> failwith("non functional value"))
-																		in let rec map (f : evT) (lista : evT list) : (evT list) = match lista with
-																				[] -> []
-																			|	hd::tl -> (apply f hd)::(map f tl)
-											in SetVal(list_as_set(map fClosure l), t)
-	| f, _ -> failwith("Not a valid set")
-)
+		| Map(f, s) -> ( match f, eval s r with
+					  f, SetVal([], t) -> failwith("Set is Empty")
+					| f, SetVal(l, t) ->  let fClosure = (eval f r) 
+								in let apply (f : evT) (v : evT) : evT = (match f with
+								  FunVal(arg, fBody, fDecEnv) -> eval fBody (bind fDecEnv arg v) 
+								| RecFunVal(g, (arg, fBody, fDecEnv)) -> let rEnv = (bind fDecEnv g f) in
+													  let aEnv = (bind rEnv arg v) in
+														eval fBody aEnv 
+								|	_ -> failwith("non functional value"))
+							in let rec map (f : evT) (lista : evT list) : (evT list) = match lista with
+											  [] -> []
+											| hd::tl -> (apply f hd)::(map f tl)
+										in SetVal(list_as_set(map fClosure l), t)
+					| f, _ -> failwith("Not a valid set"))
 		(*Funzioni di appoggio per le operazioni base*)
 
 and findmin (l : evT list) : evT = match l with
-		| [] -> Int(0)
+		  [] -> Int(0)
 		| [element] -> element
 		| element::tail -> let smallest = findmin tail in if element < smallest then element else smallest
 
 and findmax (l : evT list) : evT = match l with
-		| [] -> Int(0)
+		  [] -> Int(0)
 		| [element] -> element
 		| element::tail -> let biggest = findmax tail in if element > biggest then element else biggest
 
 and delete (toDelete : exp)(l : evT list) : (evT list) = match l with 
-																				[] -> []
-																			| element::tail -> if toDelete =  (toexp element) then (delete toDelete tail) 
+		  [] -> []
+		| element::tail -> if toDelete =  (toexp element) then (delete toDelete tail) 
 																													else element::(delete toDelete tail)													
 ;; 
 	
